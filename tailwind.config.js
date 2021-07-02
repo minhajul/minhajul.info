509f8938bfca99b65ac9d5499483c2ff2fd62b09
minhajul <kmsohelinfo@gmail.com>
2021-07-02 15:50:59 +0600

    Updated home page

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/**/*.js'
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {},
+
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
+};
