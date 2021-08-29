4561fc5a27e9e58704653b874ed95ba3019e76e5
minhajul <kmsohelinfo@gmail.com>
2021-08-29 20:11:54 +0600

    Updated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') }} - A better way to Understanding of life and living organisms</title>
+    <title>{{ config('app.name') }} - A better way to manage your money</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
