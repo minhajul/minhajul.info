ed24d2fbf4c7edc5d9c3c7d714fcd033672d6807
minhajul <kmsohelinfo@gmail.com>
2021-08-29 10:39:14 +0600

    Updated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -40,7 +40,7 @@
                         <nav class="mt-5 flex-shrink-0 h-full divide-y overflow-y-auto" aria-label="Sidebar">
                             <div class="px-2 space-y-1">
 
-                                <a href="{{ route('home') }}" class="text-white hover:text-black hover:bg-white group flex items-center px-2 py-2 text-base font-medium rounded-md" aria-current="page">
+                                <a href="{{ route('admin.home') }}" class="text-white hover:text-black hover:bg-white group flex items-center px-2 py-2 text-base font-medium rounded-md" aria-current="page">
                                     <svg class="mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                                     </svg>
@@ -97,7 +97,7 @@
                     <nav class="mt-5 flex-1 flex flex-col divide-y divide-cyan-800 overflow-y-auto" aria-label="Sidebar">
                         <div class="px-2 space-y-1">
 
-                            <a href="{{ route('home') }}" class="text-white hover:text-black hover:bg-white group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md" aria-current="page">
+                            <a href="{{ route('admin.home') }}" class="text-white hover:text-black hover:bg-white group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md" aria-current="page">
                                 <svg class="mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
                                 </svg>
