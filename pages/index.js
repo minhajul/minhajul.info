8ddded18addb06d90278e57bb19341686edb0351
minhajul <kmsohelinfo@gmail.com>
2022-02-22 11:03:48 +0600

    Added a next.js app

---
+++ pages/index.js
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import profilePic from '../public/minhajul.jpeg'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+                <title>Minhajul Islam</title>
+
+                <meta name="twitter:title" content="Minhajul Islam"/>
+                <meta name="og:title" content="Minhajul Islam"/>
+
+                <meta name="twitter:card" content="summary"/>
+                <meta name="twitter:description" content="A full-stack developer at Teleaus"/>
+                <meta name="twitter:site" content="@minhaj_cse"/>
+                <meta name="twitter:image" content="http://expense-tracker.test/images/minhajul.jpeg"/>
+
+                <meta name="og:site_name" content="A full-stack developer at Teleaus"/>
+                <meta name="og:image" content="http://expense-tracker.test/images/minhajul.jpeg"/>
+                <meta name="og:type" content="website"/>
+                <meta name="og:locale" content="en_US"/>
+            </Head>
+
+            <main>
+                <div className="relative bg-white overflow-hidden">
+                    <div className="hidden lg:block lg:absolute lg:inset-0" aria-hidden="true">
+                        <svg className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8" width="640" height="784" fill="none" viewBox="0 0 640 784">
+                            <defs>
+                                <pattern id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                    <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor"></rect>
+                                </pattern>
+                            </defs>
+                            <rect y="72" width="640" height="640" className="text-gray-50" fill="currentColor"></rect>
+                            <rect x="118" width="404" height="784" fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"></rect>
+                        </svg>
+                    </div>
+
+                    <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
+                        <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+                            <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                                <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+                                    <h2>
+                                        <span className="block text-sm font-normal tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
+                                            Hi
+                                        </span>
+                                        <span className="mt-1 block tracking-tight sm:text-5xl xl:text-6xl">
+                                            <span className="block font-extrabold text-gray-600">I'm Minhajul Islam</span>
+                                            <span className="block text-normal font-bold text-black text-3xl mt-3">
+                                            Software and Applications Engineer at <a target="_blank" className="text-blue-400 font-extrabold" href="https://teleaus.com">TELEAUS</a>
+                                            </span>
+                                        </span>
+                                    </h2>
+                                    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                                        Experienced Software Engineer with a demonstrated history of working in the
+                                        software development
+                                        industry, particularly in backend application development. Skilled in Python,
+                                        PHP, Django, DRF,
+                                        Laravel, Javascript, MySQL, Vue.js but confident and capable enough to adapt to
+                                        any stack,
+                                        platform or technology.
+                                    </p>
+
+                                    <p className="mt-6 text-gray-500">
+                                        You can also find me on <a target="_blank" className="text-black font-bold" href="https://twitter.com/minhaj_cse">Twitter</a> and
+                                        <a target="_blank" className="text-black font-bold" href="https://github.com/minhajul">Github</a>.
+                                    </p>
+                                </div>
+
+                                <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+                                    <svg className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden" width="640" height="784" fill="none" viewBox="0 0 640 784" aria-hidden="true">
+                                        <defs>
+                                            <pattern id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                                                <rect x="0" y="0" width="4" height="4" className="text-gray-200" fill="currentColor"></rect>
+                                            </pattern>
+                                        </defs>
+                                        <rect y="72" width="640" height="640" className="text-gray-50" fill="currentColor"></rect>
+                                        <rect x="118" width="404" height="784" fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"></rect>
+                                    </svg>
+                                    <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+                                        <div className="relative block w-full bg-white rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                            <Image src={profilePic} alt="Minhajul Islam"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
