95f6c1a51ea259189dd4b38b07e7e8e5eca7d973
minhajul <kmsohelinfo@gmail.com>
2025-01-16 00:17:24 +0600

    Updated

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 
 export default function Home() {
     return (
-        <div>
+        <div className="personal-blog">
             <Head>
                 <meta charSet="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
