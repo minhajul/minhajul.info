afc68dffc1c478d6f53c5e2ef1dddef2d73fcd4e
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-09-07 16:02:28 +0600

    Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import Typewriter from "@/components/Typewriter";
 
 export default function Hero(){
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-14">
+        <div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-14">
             <div className="col-span-1 lg:col-span-2">
                 <div className="text-center">
                     <Typewriter></Typewriter>
@@ -13,12 +13,14 @@ export default function Hero(){
 
                 <p className="mt-3 text-base lg:text-lg font-normal text-slate-300 sm:mt-5">
                     I am a full-stack software engineer with advanced knowledge in DevOps.
-                    I specialize in developing scalable applications using <strong>PHP</strong>, <strong>Javascript</strong>, <strong>Golang</strong>, <strong>Laravel</strong>, <strong>Livewire</strong> and modern JavaScript frameworks such as <strong>Node.js</strong>, <strong>React</strong>, and <strong>Next.js</strong>.
-                    My experience includes implementing <strong>CI/CD</strong> pipelines, <strong>Docker</strong> containerization, and deploying cloud solutions on <strong>AWS</strong>, complemented by strong database management skills in <strong>MySQL</strong> and <strong>PostgreSQL</strong>.
-                    I am particularly focused on architecting distributed systems and microservices that enable business growth and operational efficiency.
+                    I specialize in developing scalable applications using <strong>PHP</strong>, <strong>Javascript</strong>, <strong>Golang</strong>, <strong>Laravel</strong>, <strong>Livewire</strong> and JavaScript frameworks such as <strong>Node.js</strong>, <strong>React</strong>, and <strong>Next.js</strong>.
+
+                    I have hands-on experience with DevOps tools, including <strong>Docker</strong> and <strong>Kubernetes</strong>, to streamline development, deployment, and scaling processes on <strong>AWS</strong>.
+
+                    My experience also includes designing distributed systems and microservices, as well as managing databases.
 
                     <span className="block pt-5">
-                        I am eager to apply my technical expertise to deliver impactful solutions while contributing to the success of forward-thinking technology teams.
+                        I strive to apply my technical expertise to deliver impactful solutions while contributing to the success of forward-thinking technology teams.
                     </span>
                 </p>
             </div>
