5524fa9fc575777b97f824f066fb269ddc59bd21
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-09-07 12:32:38 +0600

    Updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero(){
                 </div>
 
                 <p className="mt-3 text-base lg:text-lg font-normal text-slate-300 sm:mt-5">
-                    I am a full-stack software engineer with expertise in system design and DevOps.
+                    I am a full-stack software engineer with advanced knowledge in DevOps.
                     I specialize in developing scalable applications using <strong>PHP</strong>, <strong>Javascript</strong>, <strong>Golang</strong>, <strong>Laravel</strong>, <strong>Livewire</strong> and modern JavaScript frameworks such as <strong>Node.js</strong>, <strong>React</strong>, and <strong>Next.js</strong>.
                     My experience includes implementing <strong>CI/CD</strong> pipelines, <strong>Docker</strong> containerization, and deploying cloud solutions on <strong>AWS</strong>, complemented by strong database management skills in <strong>MySQL</strong> and <strong>PostgreSQL</strong>.
                     I am particularly focused on architecting distributed systems and microservices that enable business growth and operational efficiency.
