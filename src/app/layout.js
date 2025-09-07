0863a13aedb662d7c9469b01e3b01fdc07bd16a4
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-09-07 20:36:18 +0600

    Added vercel speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import "./globals.css";
 import {Fira_Code} from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { Analytics } from '@vercel/analytics/next';
+import {Analytics} from '@vercel/analytics/next';
+import {SpeedInsights} from '@vercel/speed-insights/next';
 
 const firaCode = Fira_Code({
     subsets: ["latin"],
@@ -43,7 +44,8 @@ export default function RootLayout({children}) {
 
         <Footer/>
 
-        <Analytics />
+        <Analytics/>
+        <SpeedInsights/>
 
         </body>
         </html>
