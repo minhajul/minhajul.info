a426f73c37bf38356d56fc9315db217e0185400e
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-09-30 16:56:11 +0600

    Removed vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,6 @@ import "./globals.css";
 import {Fira_Code} from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {Analytics} from '@vercel/analytics/next';
-import {SpeedInsights} from '@vercel/speed-insights/next';
 
 const firaCode = Fira_Code({
     subsets: ["latin"],
@@ -43,9 +41,6 @@ export default function RootLayout({children}) {
         {children}
 
         <Footer/>
-
-        <Analytics/>
-        <SpeedInsights/>
         </body>
         </html>
     );
