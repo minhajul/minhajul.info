2d9bb7ec94ff08869dd87f306023174d41417ea9
Md Minhajul Islam <kmsohelinfo@gmail.com>
2025-10-21 10:31:25 +0600

    Updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
