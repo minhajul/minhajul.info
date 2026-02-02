95e7ff574007ef7542bf1c916af16785923060f7
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-02-02 23:33:31 +0600

    Updated

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -3,39 +3,7 @@
 import {motion} from "framer-motion"
 import Link from "next/link";
 import Heading from "@/components/partials/Heading";
-
-const projects = [
-    {
-        "title": "Laravel Grafana",
-        "url": "https://github.com/minhajul/laravel-grafana",
-        "technologies": ['Laravel', 'Docker', 'Grafana', 'Prometheus'],
-        "description": "Monitoring the performance of the Laravel app using Grafana, Prometheus, and Loki. Containerizing the project using Docker."
-    },
-    {
-        "title": "Docker Cleaner",
-        "url": "https://github.com/minhajul/docker-cleaner",
-        "technologies": ['Golang', 'Docker', 'Bubbletea', 'Lipgloss'],
-        "description": "This is a terminal user interface (TUI) application built with Go and BubbleTea for cleaning up Docker images and containers."
-    },
-    {
-        "title": "Go - Thundering Herd",
-        "url": "https://github.com/minhajul/go-thundering-herd",
-        "technologies": ['Golang', 'Docker', 'Kafka', 'System Design', 'PostgreSQL'],
-        "description": "A distributed system designed to send millions of emails to users across 100+ different time zones at exactly midnight their local time."
-    },
-    {
-        "title": "Kubernetes Microservices",
-        "url": "https://github.com/minhajul/kubernetes-microservices",
-        "technologies": ['Laravel', 'Next.js', 'Docker', 'Grafana', 'Prometheus', 'Loki', 'Promtail'],
-        "description": "A cloud-native application stack featuring Laravel (Backend), Next.js (Frontend), and Kubernetes, complete with a full observability suite (Loki, Grafana, Fluent Bit)."
-    },
-    {
-        "title": "Tailscale with Terraform",
-        "url": "https://github.com/minhajul/learning-terraform/tree/main/automate-tailscale",
-        "technologies": ['AWS', 'Terraform', 'Tailscale'],
-        "description": "This Terraform project provisions an AWS EC2 instance configured as a Tailscale exit node, along with all necessary networking infrastructures."
-    }
-];
+import { projects } from "@/data/projects";
 
 export default function Projects() {
     return (
