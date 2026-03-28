8a17d709e5243e85a8f2f6dc57a69b00933895e7
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-03-28 23:12:45 +0600

    fixed linting issue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+//
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+//
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals"),
+// ];
+//
+// export default eslintConfig;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    ".agents/**",
+  ]),
+]);
 
 export default eslintConfig;
