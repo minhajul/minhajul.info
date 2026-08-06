2506e4cf11c6a320adab69670e0aae1675b75a0d
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-08-06 23:10:43 +0600

    Updated

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -38,7 +38,6 @@ export const experiences: Experience[] = [
             "Built and improved CI/CD pipelines, reducing release time",
             "Introduced better tools and processes to improve team efficiency and product quality",
             "Monitored system performance and fixed bottlenecks to keep applications fast and reliable",
-            "Clearly communicated technical decisions and progress to both technical and non-technical audiences",
             "Helped resolve complex technical problems by guiding the team toward effective solutions",
         ]
     },
